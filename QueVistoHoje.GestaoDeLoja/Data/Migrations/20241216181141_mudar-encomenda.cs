5cb26942b0f5aefeaa36af9a4a0106d697a5dbd6
Vicente Cunha <vicentenmc@gmail.com>
2024-12-16 18:43:49 +0000

    Nao ta a dar para adicionar encomenda

---
+++ QueVistoHoje.GestaoDeLoja/Data/Migrations/20241216181141_mudar-encomenda.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QueVistoHoje.GestaoDeLoja.Migrations
+{
+    /// <inheritdoc />
+    public partial class mudarencomenda : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Encomendas_AspNetUsers_ClienteId",
+                table: "Encomendas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Encomendas_ClienteId",
+                table: "Encomendas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClienteId",
+                table: "Encomendas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ClienteId",
+                table: "Encomendas",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Encomendas_ClienteId",
+                table: "Encomendas",
+                column: "ClienteId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Encomendas_AspNetUsers_ClienteId",
+                table: "Encomendas",
+                column: "ClienteId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
